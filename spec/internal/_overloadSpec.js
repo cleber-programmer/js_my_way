4ab3beb48041bab10768a8d949ed37f1de429ab2
Cleber de Moraes Gonçalves <cleber.moraes@la.ad.b2w>
2016-03-09 17:47:26 -0300

    teste do modulo _overloadSpec.js

---
+++ spec/internal/_overloadSpec.js
@@ -0,0 +1,28 @@
+(function (context) {
+
+  describe('_overlaod.js', function () {
+
+    var oneOff;
+
+    beforeEach(function () {
+      oneOff = {};
+    });
+
+    it('cria sobrecarga de funcao', function () {
+
+      context._overload(oneOff, 'add', function (a, b) {
+        return a + b;
+      });
+
+      context._overload(oneOff, 'add', function (a, b, c) {
+        return a + b + c;
+      });
+
+      expect(oneOff.add(1, 2)).toEqual(3);
+      expect(oneOff.add(1, 2, 3)).toEqual(6);
+
+    });
+
+  });
+
+})(this);
