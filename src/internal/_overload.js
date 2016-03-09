91d09d4df07ab7ae26f5fc0e2bee889a5e581d95
Cleber de Moraes Gonçalves <cleber.moraes@la.ad.b2w>
2016-03-09 18:18:58 -0300

    quando nao encontrado uma funcao com o total de argumento ele deve executar a primeira funcao criada

--- src/internal/_overload.js
+++ src/internal/_overload.js
@@ -2,7 +2,7 @@ this._overload = function (a, b, c) {
 
   (function (x) {
     a[b] = function () {
-      return (c.length == arguments.length ? c : x).apply(null, arguments);
+      return (c.length == arguments.length ? c : (x || c)).apply(null, arguments);
     };
   })(a[b]);
 
