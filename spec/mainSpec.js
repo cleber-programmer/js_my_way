0899443aef70ba0a4bf5a0b5aa013c907e088c9f
Cleber de Moraes Gonçalves <cleber.moraes@la.ad.b2w>
2016-03-09 18:17:59 -0300

    teste para o modulo main.js

---
+++ spec/mainSpec.js
@@ -0,0 +1,41 @@
+(function (context) {
+
+  describe('main.js', function () {
+
+    var oneOff;
+
+    beforeEach(function () {
+
+    });
+
+    it('cria um modulo', function () {
+
+      context.$('add', [], function () {
+        return function (a, b) {
+          return a + b;
+        };
+      });
+
+      context.$(['add'], function (add) {
+        expect(add(1, 2)).toEqual(2);
+      });
+
+    });
+
+    it('cria um modulo com dependencia', function () {
+
+      context.$('hello', ['add'], function (add) {
+        return function (a) {
+          return add('hello ', a);
+        };
+      });
+
+      context.$(['hello'], function (hello) {
+        expect(hello('cleber.programmer')).toEqual('hello cleber.programmer');
+      });
+
+    });
+
+  });
+
+})(this);
