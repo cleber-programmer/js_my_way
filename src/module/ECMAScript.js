375646c7203680dfccdcf44bad833ab585f3e923
Cleber de Moraes Gonçalves <cleber.moraes@la.ad.b2w>
2016-03-10 19:14:54 -0300

    refatoracao do modulo ECMAScript.js

--- src/module/ECMAScript.js
+++ src/module/ECMAScript.js
@@ -1,21 +1,25 @@
 (function () {
   
   function build(a) {
-    (function (a) {
-      $(a, [], function (b) {
-        return b[a].apply(b, [].slice(arguments, 1));
+    (function (x) {
+
+      $(x, [], function () {
+        return function (a) {
+          return a[x].apply(a, [].slice(arguments, 1));
+        }
       });
+
     })(a);
   }
   
   function mapper(a, b) {
-    return a.concat(Object.getOwnPropertyNames(b).reduce(solve, []));
+    return a.concat(Object.getOwnPropertyNames(b).reduce(solve.bind(null, b), []));
   }
   
-  function solve(a, b) {
-    return (typeof b == 'function' && a.push(b)), a;
+  function solve(a, b, c) {
+    return (typeof a[c] == 'function' && b.push(c)), b;
   }
 
   _uniq([Object, Object.prototype, Array, Array.prototype, Number, Number.prototype, String, String.prototype].reduce(mapper, [])).forEach(build);
 
-}).call(this);
+})(this);
