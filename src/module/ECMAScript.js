472e71ef4c915a1277f9454dd92a2e9635abe171
Cleber de Moraes Gonçalves <cleber.moraes@la.ad.b2w>
2016-03-11 15:33:09 -0300

    refatoracao no modulo

--- src/module/ECMAScript.js
+++ src/module/ECMAScript.js
@@ -1,25 +1,33 @@
 (function (context) {
-  
-  function build(a) {
-    (function (x) {
 
-      context.$(x, [], function () {
-        return function (a) {
-          return a[x].apply(a, [].slice.call(arguments, 1));
-        }
-      });
+  context.$(['remove'], function (remove) {
 
-    })(a);
-  }
-  
-  function mapper(a, b) {
-    return a.concat(Object.getOwnPropertyNames(b).reduce(solve.bind(null, b), []));
-  }
-  
-  function solve(a, b, c) {
-    return (typeof a[c] == 'function' && b.push(c)), b;
-  }
+    function clear(a) {
+      return ['assign'].reduce(remove, a);
+    }
+    
+    function build(a) {
+      (function (x) {
 
-  context._uniq([Object, Object.prototype, Array, Array.prototype, Number, Number.prototype, String, String.prototype].reduce(mapper, [])).forEach(build);
+        context.$(x, [], function () {
+          return function (a) {
+            return a[x].apply(a, [].slice.call(arguments, 1));
+          }
+        });
+
+      })(a);
+    }
+    
+    function mapper(a, b) {
+      return a.concat(Object.getOwnPropertyNames(b).reduce(solve.bind(null, b), []));
+    }
+    
+    function solve(a, b, c) {
+      return (typeof a[c] == 'function' && b.push(c)), b;
+    }
+
+    clear(context._uniq([Object, Object.prototype, Array, Array.prototype, Number, Number.prototype, String, String.prototype].reduce(mapper, []))).forEach(build);
+
+  });
 
 })(this);
