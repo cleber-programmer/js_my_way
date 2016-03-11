91fd358f5b4d41d725a5fee9845a209173d5c23c
Cleber de Moraes Gonçalves <cleber.moraes@la.ad.b2w>
2016-03-11 15:32:13 -0300

    inclusao da funcao que remove as funcoes que nao podem ser automatizadas

--- spec/module/ECMAScriptSpec.js
+++ spec/module/ECMAScriptSpec.js
@@ -4,16 +4,7 @@
 
     describe('Object', function () {
 
-      // it('funcao assign foi definida', function (done) {
-
-      //   context.$(['assign'], function (assign) {
-
-      //     expect(assign({}, { a: 0 })).toEqual({ a: 0 });
-      //     done();
-
-      //   });
-
-      // });
+      
 
     });
 
