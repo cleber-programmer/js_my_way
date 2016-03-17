6c8280a808a8a77a23efd544a186b15ef31d2799
Cleber de Moraes Gonçalves <cleber.moraes@la.ad.b2w>
2016-03-17 15:39:32 -0300

    inclusao do modulo lambda

---
+++ src/module/lambda.js
@@ -0,0 +1,20 @@
+this.Rex('lambda', [
+
+  'apply',
+  'concat',
+  'curry',
+  'format',
+  'match',
+  'partial',
+  'split',
+  '_'
+
+], function (apply, concat, curry, format, match, partial, split, _) {
+
+  function lambda(arrow, expression) {
+    return apply(Function, concat(split(arrow(expression)[1], ','), [format('return {0}', [arrow(expression)[2]])]));
+  }
+  
+  return partial(lambda, [curry(match)(_, /^\((.*)\)\s*=>\s*(.*)$/)]);
+
+});
