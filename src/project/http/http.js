5a8dbc5a7e3f87e4272dd95ac59af2c362ad7b27
Cleber de Moraes Gonçalves <cleber.moraes@b2wdigital.com>
2016-03-21 16:35:21 -0300

    inclusao do modulo de http

---
+++ src/project/http/http.js
@@ -0,0 +1,29 @@
+Rex('http', [
+
+  'always',
+  'equal',
+  'get',
+  'or',
+  'set',
+  'http.interceptor'
+
+], function (always, equal, get, or, set, interceptor) {
+  
+  return function (method, url, data) {
+    return (function (listeners, xhr) {
+
+      xhr.onreadystatechange = function () {
+        equal(get(this, 'readyState'), 4) && or(get(listeners, get(this, 'status')), always)(interceptor('response', get(this, 'responseText'), this), this);
+      };
+      
+      xhr.open(method, url, !0);
+      xhr.send(interceptor('request', JSON.stringify(or(data, '')), xhr));
+
+      return function when(status, callback) {
+        return set(listeners, status, callback), when;
+      }
+
+    })({}, new XMLHttpRequest());    
+  };
+  
+});
