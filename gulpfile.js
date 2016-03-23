c4561489098ad13316edac0025b39b0451e5e8e4
Cleber de Moraes Gonçalves <cleber.moraes@b2wdigital.com>
2016-03-23 17:23:21 -0300

    inclusao do gulp

---
+++ gulpfile.js
@@ -0,0 +1,41 @@
+var concat  = require('gulp-concat');
+var gulp    = require('gulp');
+var uglify  = require('gulp-uglify');
+
+gulp.task('main', function () {
+  
+  return gulp.src(['./src/internal/*.js', './src/main.js'])
+             .pipe(concat('rex.min.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest('./dist/'));
+  
+});
+
+gulp.task('module', function () {
+  
+  return gulp.src('./src/module/*.js')
+             .pipe(concat('rex-module.min.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest('./dist/'));
+  
+});
+
+gulp.task('h', function () {
+  
+  return gulp.src('./src/project/h/*.js')
+             .pipe(concat('rex-h.min.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest('./dist/'));
+  
+});
+
+gulp.task('http', function () {
+  
+  return gulp.src('./src/project/http/*.js')
+             .pipe(concat('rex-http.min.js'))
+             .pipe(uglify())
+             .pipe(gulp.dest('./dist/'));
+  
+});
+
+gulp.task('default', ['main', 'module', 'h', 'http']);
