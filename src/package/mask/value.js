06c127a70c395b9f53156c298bcfb5a7836310c4
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2016-04-01 21:17:27 +0000

    refatoracao por causa de bus desconhecido

--- src/package/mask/value.js
+++ src/package/mask/value.js
@@ -1,19 +1,24 @@
-Rex('mask.value', ['mask.target'], function (target) {
+Rex('mask.value', [
+  
+    'join'
+  , 'push'
+  , 'splice'
+  , 'mask.target'
+  
+], function (join, push, splice, target) {
   
   return function (event) {
-     
-    if (!(this instanceof arguments.callee)) {
-      return new arguments.callee(event);
-    }
-
-    [].push.apply(this, [].slice.call(target(event).value));
-    
-    if (event.keyCode) {
-      [].splice.call(this, target(event).selectionStart, 1, String.fromCharCode((96 <= event.keyCode && event.keyCode <= 105 ? event.keyCode - 48 : event.keyCode)));
-    }
-    
-    return [].join.call(this, '');
-    
+    return (function (chars) {
+      
+      push(chars, [].slice.call(target(event).value));
+      
+      if (event.keyCode) {
+        splice(chars, target(event).selectionStart, 1, String.fromCharCode((96 <= event.keyCode && event.keyCode <= 105 ? event.keyCode - 48 : event.keyCode)));
+      }
+      
+      return join(chars, '');
+      
+    })([]);
   };
   
 });
