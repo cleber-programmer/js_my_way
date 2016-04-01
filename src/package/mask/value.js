c4618800d4d985922f397f68ad3351c110e15440
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2016-04-01 20:50:03 +0000

    nao posso utilizar nenhum modulo na package mask

--- src/package/mask/value.js
+++ src/package/mask/value.js
@@ -1,25 +1,18 @@
-Rex('mask.value', [
-  
-    'join'
-  , 'slice'
-  , 'splice'
-  , 'mask.target'
-  
-], function (join, slice, splice, target) {
+Rex('mask.value', ['mask.target'], function (target) {
   
   return function (event) {
-      
+     
     if (!(this instanceof arguments.callee)) {
       return new arguments.callee(event);
     }
 
-    [].push.apply(this, slice(target(event).value));
+    [].push.apply(this, [].slice.call(target(event).value));
     
     if (event.keyCode) {
-      splice(this, target(event).selectionStart, 1, String.fromCharCode((96 <= event.keyCode && event.keyCode <= 105 ? event.keyCode - 48 : event.keyCode)));
+      [].splice.call(this, target(event).selectionStart, 1, String.fromCharCode((96 <= event.keyCode && event.keyCode <= 105 ? event.keyCode - 48 : event.keyCode)));
     }
     
-    return join(this, '');
+    return [].join.call(this, '');
     
   };
   
