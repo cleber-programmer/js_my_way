ee6fab546ffc14a51fce8d12ef33840dd79c97bf
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2016-04-13 16:27:53 +0000

    refatoracao no modulo dobounce

--- src/module/dobounce.js
+++ src/module/dobounce.js
@@ -1,10 +1,15 @@
-Rex('dobounce', ['partial'], function (partial) {
+Rex('dobounce', [
+  
+    'partial'
+  , 'slice'
+  
+], function (partial, slice) {
   
   return function (wait, callback, timeoutID) {
     return function () {
         (timeoutID = window.clearTimeout(timeoutID))
-      , (timeoutID = window.setTimeout(partial(callback, arguments), wait));
-    }
+      , (timeoutID = window.setTimeout(partial(callback, slice(arguments)), wait));
+    };
   };
   
 });
