5ed7dfeec26b55c44c3f8de898f0acf2f3faf36f
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2016-04-28 19:14:44 +0000

    extendendo o modulo $

--- src/module/$.js
+++ src/module/$.js
@@ -1,7 +1,15 @@
-Rex('$', [], function () {
+Rex('$', ['$$', 'get', 'or'], function ($$, or) {
   
-  return function (selector) {
-    return document.querySelector(selector);
+  return function (selector, node) {
+    return get($$(selector, node), 0, null);
+  };
+  
+});
+
+Rex('$$', ['or'], function (or) {
+  
+  return function (selector, node) {
+    return or(node, document).querySelectoAll(selector);
   };
   
 });
