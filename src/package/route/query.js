690db9fb295cb2e70f0b1206e398df140685fbe8
Cleber de Moraes Goncalves <cleber.programmer@hotmail.com>
2016-05-12 14:56:34 +0000

    retirada do join, antes o slice stava funcionando da forma errada

--- src/package/route/query.js
+++ src/package/route/query.js
@@ -1,7 +1,6 @@
 Rex('route.query', [
   
-    'join'
-  , 'lambda'
+    'lambda'
   , 'get'
   , 'map'
   , 'reduce'
@@ -9,14 +8,14 @@ Rex('route.query', [
   , 'slice'
   , 'split'
   
-], function (join, lambda, get, map, reduce, set, slice, split) {
+], function (lambda, get, map, reduce, set, slice, split) {
   
   function callback(query, item) {
     return set(query, get(item, 0), get(item, 1, ''));
   }
   
   return function () {
-    return reduce(map(split(join(slice(window.location.search, 1), ''), '&'), lambda('(a) => a.split("=")')), callback, {});
+    return reduce(map(split(slice(window.location.search, 1), '&'), lambda('(a) => a.split("=")')), callback, {});
   };
   
 });
