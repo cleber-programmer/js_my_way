c6481812f55ba9c11713f0fd15d8a21e529b39ac
cleber.goncalves <cleber.goncalves@concrete.com.br>
2016-08-30 13:01:48 -0300

    refatoracao de codigo e teste unitario

--- dist/rex.js
+++ dist/rex.js
@@ -237,25 +237,7 @@ Rex('curry', function (_ref) {
 		}
 	);
 });
-'use strict';
-
-/* global Rex */
-/* jshint esversion: 6 */
-
-Rex('defaultTo', function (_ref) {
-	var curry = _ref.curry;
-	var or = _ref.or;
-	return (
-
-		/**
-   * Retorna o segundo argumento, se nao for null, indefinido ou NaN
-   * caso contrario, o primeiro argumento e retornado
-   */
-		curry(function (defaultValue, value) {
-			return or(defaultValue, value);
-		})
-	);
-});
+"use strict";
 'use strict';
 
 /* global Rex */
@@ -294,24 +276,7 @@ Rex('f', function (_ref) {
 		always(!1)
 	);
 });
-'use strict';
-
-/* global Rex */
-/* jshint esversion: 6 */
-
-Rex('getter', function (_ref) {
-	var or = _ref.or;
-	return (
-
-		/**
-   * Retorna o valor de uma chave de um objeto ou
-   * retorna o valor default
-   */
-		function (object, property, defaultValue) {
-			return or(or(object, {})[property], defaultValue);
-		}
-	);
-});
+"use strict";
 'use strict';
 
 var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
--- test/src/vDOM/vRepaint.spec.js
+++ test/src/vDOM/vRepaint.spec.js
@@ -3,7 +3,26 @@ describe('vDOM', () => {
   describe('vRepaint', () => {
 
     beforeEach(() =>
-      (document.body.innerHTML = '<table id="fruits"></table>'));
+      (document.body.innerHTML = `
+        <table id="fruits">
+          <th>
+            <td>letter</td>
+            <td>fruit</td>
+          </th>
+          <tr>
+            <td>a</td>
+            <td>apple</td>
+          </tr>
+          <tr>
+            <td>b</td>
+            <td>banana</td>
+          </tr>
+          <tr>
+            <td>c</td>
+            <td>cherry</td>
+          </tr>
+        </table>
+      `));
 
     it('Verificando a existencia do modulo vRepaint', () => {
 
@@ -12,25 +31,120 @@ describe('vDOM', () => {
 
     });
 
-    it('Extendendo um tag html', () => {
+    it('Removendo um elemento html', () => {
 
-      Rex(({ vRepaint, vTag: { table }, $ }) => {
+      Rex(({ vRepaint, vTag: { table, td, th, tr }, vText, $ }) => {
 
-        vRepaint($('#fruits'), table());        
-        expect($('#fruits').tagName).toBe('TABLE');
+        vRepaint($('#fruits'), table([
+          th([
+            td(vText('letter')),
+            td(vText('fruit'))
+          ]),
+          tr([
+            td(vText('a')),
+            td(vText('apple'))
+          ]),
+          tr([
+            td(vText('b')),
+            td(vText('banana'))
+          ])
+        ]));
+
+        expect($('#fruits').childNodes.length).toBe(3);
 
       });
 
     });
 
-    it('Substituindo um tag html', () => {
+    it('Adicionando um novo elemento html', () => {
 
-      Rex(({ vRepaint, vTag: { tr }, $ }) => {
+      Rex(({ vRepaint, vTag: { table, td, th, tr }, vText, $ }) => {
 
-        $('#fruits').appendChild(document.createElement('div'));
+        vRepaint($('#fruits'), table([
+          th([
+            td(vText('letter')),
+            td(vText('fruit'))
+          ]),
+          tr([
+            td(vText('a')),
+            td(vText('apple'))
+          ]),
+          tr([
+            td(vText('b')),
+            td(vText('banana'))
+          ]),
+          tr([
+            td(vText('c')),
+            td(vText('cherry'))
+          ]),
+          tr([
+            td(vText('d')),
+            td(vText('durian'))
+          ])
+        ]));
 
-        vRepaint($('#fruits div'), tr());
-        expect($('#fruits tr').tagName).toBe('TR');
+        expect($('#fruits').childNodes.length).toBe(5);
+
+      });
+
+    });
+
+    it('Trocando um elemento html', () => {
+
+      Rex(({ vRepaint, vTag: { table, td, th, tr }, vText, $ }) => {
+
+        expect($('#fruits').childNodes[0].tagName).toBe('TH');
+
+        vRepaint($('#fruits'), table([
+          tr([
+            td(vText('letter')),
+            td(vText('fruit'))
+          ]),
+          tr([
+            td(vText('a')),
+            td(vText('apple'))
+          ]),
+          tr([
+            td(vText('b')),
+            td(vText('banana'))
+          ]),
+          tr([
+            td(vText('c')),
+            td(vText('cherry'))
+          ])
+        ]));
+
+        expect($('#fruits').childNodes[0].tagName).toBe('TR');
+
+      });
+
+    });
+
+    it('Extendendo um elemento html', () => {
+
+      Rex(({ vRepaint, vTag: { table, td, th, tr }, vText, $ }) => {
+
+        vRepaint($('#fruits'), table({ id: 'new_id' }, [
+          th([
+            td(vText('letter')),
+            td(vText('fruit'))
+          ]),
+          tr([
+            td(vText('a')),
+            td(vText('apple'))
+          ]),
+          tr([
+            td(vText('b')),
+            td(vText('banana'))
+          ]),
+          tr([
+            td(vText('c')),
+            td(vText('cherry'))
+          ])
+        ]));
+
+        expect($('#fruits')).toBe(null);
+        expect($('#new_id')).toBeDefined();
 
       });
 
